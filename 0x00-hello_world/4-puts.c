db5b012aa32e92e3bac48275ef3e91f083a774c5
Tsedexashu08 <ashutsedex@gmail.com>
2022-09-10 09:52:51 -0900

    helloworld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
  */
 int main()
 {
-	puts ("Programming is like building a multilingual puzzle.")
+	puts ("\"Programming is like building a multilingual puzzle.");
 
-		return(0)
+
+		return(0);
 }
  
