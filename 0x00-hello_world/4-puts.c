2b8d133137794f076d5ff40bb9bad7dd3d13ee60
Tsedexashu08 <ashutsedex@gmail.com>
2022-09-10 09:49:49 -0900

    helloworld

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main-prints "Programming is like building a multilingual puzzle."
+ *
+ * Returns-always 0
+ */
+int main()
+{
+	puts "Programming is like building a multilingual puzzle."
+
+		return(0)
+}
+ 
