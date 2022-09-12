b114ec6f625b9aa99562b835ba028690a5309260
Tsedexashu08 <ashutsedex@gmail.com>
2022-09-12 09:22:23 -0900

    variables and if else

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,11 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 89; num++)
 	{
 		putchar((num % 10) + '0');
-		if (num == 9)
+		if (num == 89)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
