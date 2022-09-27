84293d90c4eabca22b79169521f58456d3309125
Tsedexashu08 <ashutsedex@gmail.com>
2022-09-27 01:39:10 -0900

    filling

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,4 @@
-/*
- * File: 0-memset.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
  * memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
